a46ad7c88272fe65dd586b7d8d73c1ff7bee0d8b
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-02-18 18:02:36 -0300

    mail bell top

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Referencias
+
+https://shadcnuikit.com/dashboard/default
+https://www.shadcnblocks.com/admin-dashboard
+
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
 ## Getting Started
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import {AppSidebar} from "@/components/app-sidebar";
 import {Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator} from "@/components/ui/breadcrumb";
 import {Separator} from "@/components/ui/separator";
 import {SidebarInset, SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar";
+import {Bell, Mail} from "lucide-react";
 
 export default function Page() {
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <header className="flex place-content-between h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
@@ -23,6 +24,20 @@ export default function Page() {
                 </BreadcrumbItem>
               </BreadcrumbList>
             </Breadcrumb>
+          </div>
+          <div className="flex items-center gap-2 px-4">
+            <button className="relative inline-flex items-center justify-center text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:underline h-9 rounded-md px-3 text-foreground">
+              <Mail className="h-5 w-5" />
+              <div className="absolute bottom-[calc(100%-10px)] left-[calc(100%-12px)] h-4 w-4 flex items-center justify-center rounded-full bg-primary text-primary-foreground text-[8px] font-semibold">
+                3
+              </div>
+            </button>
+            <button className="relative inline-flex items-center justify-center text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:underline h-9 rounded-md px-3 text-foreground">
+              <Bell className="h-5 w-5" />
+              <div className="absolute bottom-[calc(100%-10px)] left-[calc(100%-12px)] h-4 w-4 flex items-center justify-center rounded-full bg-primary text-primary-foreground text-[8px] font-semibold">
+                2
+              </div>
+            </button>
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
